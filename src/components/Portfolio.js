ee6989b78fd09206a56f76acee0148d39e943ec5
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-05-15 12:41:20 +0000

    Add BnB

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Container} from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { BsGithub } from "react-icons/bs";
 import styles from "../styles/Portfolio.module.css";
 import img1 from "../assets/sh.png";
-import img2 from "../assets/locations.png";
-import img3 from "../assets/filmstar.png";
+import img2 from "../assets/BnB.png";
 
 const Portfolio = () => {
   return (
@@ -11,28 +11,53 @@ const Portfolio = () => {
       <h2 className={styles.Heading}>PORTFOLIO</h2>
       <div className={styles.Projects}>
         <div className={styles.Project}>
-          <a href="https://hemenhk.github.io/strength-house/index.html" rel="noreferrer" target="_blank">
-            <img src={img1} alt="Project 1" />
-            <div className={styles.Overlay}>
-              <p className={styles.Text}>STRENGTH HOUSE</p>
+          <img src={img1} alt="Project 1" />
+          <div className={styles.Overlay}>
+            <div className={styles.Text}>
+              <p>
+                {" "}
+                <a
+                  href="https://hemenhk.github.io/strength-house/index.html"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Strength House
+                </a>
+              </p>
+              <a
+                href="https://github.com/Hemenhk/strength-house"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <BsGithub className={styles.github} />
+              </a>
             </div>
-          </a>
+          </div>
         </div>
         <div className={styles.Project}>
-          <a href="https://locations-p5.herokuapp.com/" rel="noreferrer" target="_blank">
-            <img src={img2} alt="Project 2" />
-            <div className={styles.Overlay}>
-              <p className={styles.Text}>LOCATIONS</p>
+          <img src={img2} alt="Project 2" />
+          <div className={styles.Overlay}>
+            <div className={styles.Text}>
+              <p>
+                {" "}
+                <a
+                  href="https://hemenhk.github.io/bnb/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  BnB
+                </a>
+              </p>
+
+              <a
+                href="https://github.com/Hemenhk/bnb"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <BsGithub className={styles.github} />
+              </a>
             </div>
-          </a>
-        </div>
-        <div className={styles.Project}>
-          <a href="https://filmstar22.herokuapp.com/" rel="noreferrer" target="_blank">
-            <img src={img3} alt="Project 3" />
-            <div className={styles.Overlay}>
-              <p className={styles.Text}>FILMSTAR</p>
-            </div>
-          </a>
+          </div>
         </div>
       </div>
     </Container>
